a47175a4b5db69d9da29fd583e537e9cc305c928
Den <90329284+zadumka@users.noreply.github.com>
2023-03-23 16:49:14 +0200

    add file

---
+++ study.py
@@ -0,0 +1,14 @@
+list = [1, 2, 3, 4, 5]
+print(list)
+print(list[0])
+print(list[:2])
+
+list_sum = 0
+for num in list :
+    list_sum = list_sum + num
+print(list_sum)
+
+list1 = ['b', 'a', 'c', 'd']
+print(list1)
+list1.sort()
+print(list1)
